f2c74f6aeeb7fd8cfc512c9d3a6298be4d193ac1
eranws <eranws@gmail.com>
2014-09-30 11:23:01 +0200

    safe: wrapped main in try/catch

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,20 @@
 //========================================================================
 int main( ){
 
-    ofAppGlutWindow window;
+	ofAppGlutWindow window;
 	ofSetupOpenGL(&window, 960,768, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofRunApp( new testApp());
 
+	try
+	{
+		testApp* app = new testApp();
+		ofRunApp(app);
+	}
+	catch(exception e)
+	{
+		cout << e.what();
+	}
 }
