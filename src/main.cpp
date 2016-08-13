f03eaffe49e3a48dc02706897c25aa9c7a54f8b2
eranws <eranws@gmail.com>
2016-08-12 21:10:05 -0400

    fullScreen default: windowed on debug

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,19 @@
 int main( ){
 
 
+	ofWindowMode windowMode;
+#ifdef DEBUG
+	windowMode = OF_WINDOW;
+#else
+	windowMode = OF_FULLSCREEN;
+#endif
+
 #ifdef TARGET_WIN32
 	ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1024,768, OF_FULLSCREEN);			// <-------- setup the GL context
+	ofSetupOpenGL(&window, 1024,768, windowMode);			// <-------- setup the GL context
 #endif
 #ifdef TARGET_OSX
-	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(1024,768, windowMode);			// <-------- setup the GL context
 #endif
 #ifdef TARGET_LINUX
 	// TODO
