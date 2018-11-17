6d3d5282e7ab0866119c14899f624690f71c0c65
dstavy <dstavy@gmail.com>
2018-11-17 13:08:03 +0100

    chsnge

--- src/Watchdog_Responder.h
+++ src/Watchdog_Responder.h
@@ -68,7 +68,7 @@ public:
 		// ping to watchDog
 		if (b_use_watchdog)
 		{
-			if (ofGetElapsedTimef() - last_ping_time > 10)
+			if (ofGetElapsedTimef() - last_ping_time > 0.5)
 			{
 				ofxOscMessage m;
 				m.setAddress("/imok");
